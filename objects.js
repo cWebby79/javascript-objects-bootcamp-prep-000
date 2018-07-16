82fd1e23dffa0863cdba5fd354171649cd1aee53
Chris Webster <cgw57@hotmail.com>
2018-07-16 13:04:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {artist: 'song'};
 function updatePlaylist(obj, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
+
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+}
